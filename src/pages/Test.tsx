27e6b133429bea52b43f0450bb307f4791ab42a3
BrayRPGs <brayanymelquisedec@gmail.com>
2025-12-03 20:51:39 +0000

    Add input elements and enhance glass button styles in Test component

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -3,6 +3,28 @@ const Test: React.FC = () => {
     <div>
       <p>page for Test</p>
       <button className="glass">Click me</button>
+      <input type="button" value="Click me" className="glass" />
+      <input type="checkbox" name="testCheckbox" id="testCheckbox" className="glass" />
+      <input type="color" name="" id="" className="glass" />
+      <input type="date" name="" id="" className="glass" />
+      <input type="datetime-local" name="" id="" className="glass" />
+      <input type="email" name="" id="" className="glass" />
+      <input type="file" name="" id="" className="glass" />
+      <input type="month" name="" id="" className="glass" />
+      <input type="hidden" name="" id="" className="glass" />
+      <input type="image" name="" id="" className="glass" />
+      <input type="number" name="" id="" className="glass" />
+      <input type="password" name="" id="" className="glass" />
+      <input type="radio" name="" id="" className="glass" />
+      <input type="range" name="" id="" className="glass" />
+      <input type="reset" name="" id="" className="glass" />
+      <input type="search" name="" id="" className="glass" />
+      <input type="submit" name="" id="" className="glass" />
+      <input type="tel" name="" id="" className="glass" />
+      <input type="text" name="" id="" className="glass" />
+      <input type="time" name="" id="" className="glass" />
+      <input type="url" name="" id="" className="glass" />
+      <input type="week" name="" id="" className="glass" />
     </div>
   )
 }
